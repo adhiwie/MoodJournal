530a2cd59a16e866d7d686e8c3d0b1476e24097b
adhiwie <mail@adhiwie.web.id>
2017-09-29 15:02:36 +0100

    allow anonymous sign in

--- app/src/main/java/com/adhiwie/moodjournal/MainActivity.java
+++ app/src/main/java/com/adhiwie/moodjournal/MainActivity.java
@@ -15,6 +15,7 @@ import android.os.Handler;
 import android.os.ResultReceiver;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.constraint.ConstraintLayout;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.TaskStackBuilder;
 import android.support.v7.app.AlertDialog;
--- app/src/main/java/com/adhiwie/moodjournal/SignUpActivity.java
+++ app/src/main/java/com/adhiwie/moodjournal/SignUpActivity.java
@@ -6,6 +6,7 @@ import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.support.annotation.NonNull;
 import android.support.constraint.ConstraintLayout;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -20,9 +21,11 @@ import android.widget.TimePicker;
 
 import com.adhiwie.moodjournal.model.UserData;
 import com.adhiwie.moodjournal.service.KeepAppRunning;
-import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.ErrorCodes;
 import com.firebase.ui.auth.IdpResponse;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -31,7 +34,6 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Map;
 
@@ -150,17 +152,35 @@ public class SignUpActivity extends AppCompatActivity {
             action.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
+                    signInAnonymously();
+                    /*
                     startActivityForResult(
                             AuthUI.getInstance()
                                     .createSignInIntentBuilder()
                                     .setAvailableProviders(
-                                            Arrays.asList(new AuthUI.IdpConfig.Builder(AuthUI.PHONE_VERIFICATION_PROVIDER).build())
+                                            Arrays.asList(new AuthUI.IdpConfig.Builder(AuthUI.).build())
                                     ).build(),
                             REQUEST_CODE_SIGN_IN
                     );
+                    */
                 }
             });
         }
+    }
+
+    private void signInAnonymously() {
+        mAuth.signInAnonymously()
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            writeUserDataToFirebase(user);
+                        } else {
+                            showSnackbar("Authentication failed.");
+                        }
+                    }
+                });
     }
 
     public static class TimePickerFragment extends DialogFragment
